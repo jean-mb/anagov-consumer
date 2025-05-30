ef85126430840ad33683f8a5e4f2b5cfc68d3e9f
Jean Moschen <jean.buss@itaipuparquetec.org.br>
2025-05-30 01:00:09 -0300

    Refactor estacoes

--- src/services/estacao.py
+++ src/services/estacao.py
@@ -4,16 +4,27 @@ from dotenv import load_dotenv
 from src.utils.utils import abrir_chrome, limpar_terminal
 import gmplot
 from bs4 import BeautifulSoup
+from urllib.parse import urlencode
+
+from src.utils.plot_mapa import plotar_estacoes
 
 
 load_dotenv()
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
-def listar_estacoes(token: str):
-    url = (
-        "https://www.ana.gov.br/hidrowebservice/EstacoesTelemetricas/"
-        "HidroInventarioEstacoes/v1?C%C3%B3digo%20da%20Bacia=8"
-    )
+def get_estacoes(token: str, filtros: tuple = None):
+    base_url = "https://www.ana.gov.br/hidrowebservice/EstacoesTelemetricas/HidroInventarioEstacoes/v1"
+    param_names = [
+        "Código da Estação",
+        "Unidade Federativa",
+        "Código da Bacia"
+    ]
+    params = {
+        name: value for name, value in zip(param_names, filtros or ()) if value
+    }
+    
+    query_string = urlencode(params)
+    url = f"{base_url}?{query_string}" if query_string else base_url
     headers = {
         "accept": "*/*",
         "Authorization": f"Bearer {token}"
@@ -28,88 +39,89 @@ def listar_estacoes(token: str):
     if not items:
         print(response_json.get("message", "Nenhuma estação encontrada"))
         return None
-
-    # Filtra estações do rio 86001000
-    estacoes = []
-    for item in items:
-        if item.get('Municipio_Codigo') == "24157000" and item.get("Operando") == "1":
-            try:
-                lat = float(item['Latitude'])
-                lng = float(item['Longitude'])
-                nome = item['codigoestacao']
-                estacoes.append((lat, lng, nome))
-            except (ValueError, TypeError):
-                continue  # Ignora entradas inválidas
-
-    gmap = gmplot.GoogleMapPlotter(estacoes[0][0], estacoes[0][1], 10, apikey=GOOGLE_API_KEY)
-
-    for lat, lng, title in estacoes:
-        gmap.marker(lat, lng, title=title, color='red')
-        
-    mapa_path = "html/index.html"
-    gmap.draw(mapa_path)
-    script_cru = '''
-    setTimeout(function() {
-        console.log('Mapa carregado');
-        document.querySelectorAll('div[role="img"]').forEach(function(div) {
-            div.addEventListener('click', function() {
-                const title = div.getAttribute('title') || 'Sem título';
-                navigator.clipboard.writeText(title).then(function() {
-                    alert('Código da estação copiado: ' + title);
-                }, function(err) {
-                    console.error('Erro ao copiar: ', err);
-                });
-            });
-        });
-    }, 5000);
-    '''
-    with open(mapa_path, 'r', encoding='utf-8') as file:
-        soup = BeautifulSoup(file, 'html.parser')
-
-    script_onclick = soup.new_tag('script', type='text/javascript')
-    script_onclick.string = script_cru
-
-    if soup.body:
-        soup.body.append(script_onclick)
-    else:
-        print("A tag <body> não foi encontrada no arquivo HTML.")
-
-    with open(mapa_path, 'w', encoding='utf-8') as file:
-        file.write(str(soup))
-
-    print(f"Mapa gerado com sucesso: {mapa_path}")
-    return response_json
-
-
-def get_estacao(codigo_estacao: str, token: str):
-    url = (
-        "https://www.ana.gov.br/hidrowebservice/EstacoesTelemetricas/"
-        f"HidroInventarioEstacoes/v1?C%C3%B3digo%20da%20Esta%C3%A7%C3%A3o={codigo_estacao}"
-    )
-
-    headers = {
-        "accept": "*/*",
-        "Authorization": f"Bearer {token}"
-    }
-
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        response_json = response.json()
-        if len(response_json["items"]) != 0:
-            return response.json()
-        else:
-            print(response_json["message"])
-            return None
-    else:
-        print(f"Erro {response.status_code}: {response.text}")
-        return None
+    
+    return response
 
 def get_estacao_coords(estacao: dict):
     items = estacao["items"][0]
     print(f"\n{items['Latitude']} {items['Longitude']}\n")
     url = f"https://www.google.com/maps/search/?api=1&query={items['Latitude']},{items['Longitude']}"
     abrir_chrome(url)
+
+
+def menu_listar_estacoes(token):
+    limpar_terminal()
+    print("=" * 40)
+    print("Filtros")
+    codigo_estacao = str(input("Digite o código da estação (ou deixe em branco para todas): "))
+    bacia = str(input("Digite o código da bacia (ou deixe em branco para todas): "))
+    estado = str(input("Digite o código do estado (ou deixe em branco para todos): "))
+    filtros = (bacia, estado, codigo_estacao)
+    print("=" * 40)
+    print("\n")
+    return get_estacoes(token, filtros)
+    
+def listar_estacoes(estacoes):
+    estacoes_json = estacoes.json()
+    items = estacoes_json.get("items", [])
+    if not estacoes:
+        print("Nenhuma estação encontrada.")
+        return
+
+    print("=" * 40)
+    print("Lista de Estações:")
+    for estacao in items:
+        print(f"{estacao['codigoestacao']} - {estacao['Tipo_Estacao']} em {estacao['Municipio_Nome']}: {estacao['Estacao_Nome']}")
+        
+    print("=" * 40)
+    print("\n")
+    # print(estacoes)
+    with open("output/estacoes/estacoes.txt", "w") as file:
+        for estacao in items:
+            file.write(f"{estacao['codigoestacao']} - {estacao['Tipo_Estacao']} em {estacao['Municipio_Nome']}: {estacao['Estacao_Nome']}\n")
+    with open("output/estacoes/estacoes.json", "w") as file:
+        file.write(estacoes.text)
+
+def menu_acoes_estacoes(estacoes):
+    limpar_terminal()
+    print("=" * 40)
+    print("Ações:")
+    print(
+        "1 - Gerar mapa com estações filtradas\n"
+        "2 - Listar todas as estações\n"
+        "3 - Ver coordenadas da estação específica\n"
+        "4 - Ver detalhes da estação específica\n"
+        "0 - Voltar\n"
+    )
+    print("=" * 40)
+    print("\n")
+    opcao = str(input("Digite a opção: "))
+    match opcao:
+        case "1":
+            plotar_estacoes(estacoes)
+        case "2":
+            listar_estacoes(estacoes)
+        case "3":
+            codigo_estacao = str(input("Digite o código da estação: "))
+            estacao = next((e for e in estacoes if e['codigoestacao'] == codigo_estacao), None)
+            if estacao:
+                get_estacao_coords(estacao)
+            else:
+                print("Estação não encontrada.")
+        case "4":
+            codigo_estacao = str(input("Digite o código da estação: "))
+            estacao = next((e for e in estacoes if e['codigoestacao'] == codigo_estacao), None)
+            if estacao:
+                print(estacao)
+            else:
+                print("Estação não encontrada.")
+        case "0":
+            return
+        case _:
+            print("Opção inválida, tente novamente.")
+            limpar_terminal()
+    # listar_estacoes(token, filtros)
+
 
 def menu_estacoes(token):
     codigo_estacao = 0
@@ -119,31 +131,14 @@ def menu_estacoes(token):
         # print(f"\n{estacao['codigoestacao']} - Estação {estacao['Tipo_Estacao']} em {estacao['Municipio_Nome']}: {resultado['items'][0]['Estacao_Nome']}\n")
         print("O que deseja fazer?")
         print(
-            "1 - Ver estação específica\n"
-            "2 - Listar estações em mapa\n"
-            "3 - Próxima estação\n"
+            "1 - Listar estações\n"
             "0 - Sair\n"
         )
         opcao = str(input("Digite a opção: "))
         match opcao:
             case "1":
-                if codigo_estacao == 0:
-                    codigo_estacao = str(input("Digite o código da estação: "))
-                if codigo_estacao.lower() == "q":
-                    break
-                if not codigo_estacao.isdigit() or codigo_estacao is None or codigo_estacao == "":
-                    codigo_estacao = 0
-                    continue
-                try:
-                    resultado = get_estacao(codigo_estacao, token)
-                    codigo_estacao = 0 
-                    if resultado is None:
-                        continue
-                except Exception as e:
-                    print(e)
-                get_estacao_coords(resultado)
-            case "2":
-                listar_estacoes(token)
+                estacoes = menu_listar_estacoes(token)
+                menu_acoes_estacoes(estacoes)
             case "0":
                 break
             case _:
---
+++ src/utils/plot_mapa.py
@@ -0,0 +1,63 @@
+import gmplot
+import os
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+
+load_dotenv()
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+def plotar_estacoes(estacoes): 
+    estacoes_json = estacoes.json()
+    items = estacoes_json.get("items", [])
+    estacoes_filtradas = []
+    for item in items:
+        if item["Operando"] == "1":
+            try:
+                lat = float(item['Latitude'])
+                lng = float(item['Longitude'])
+                nome = item['codigoestacao']
+                tipo = item['Tipo_Estacao']
+                estacoes_filtradas.append((lat, lng, nome, tipo))
+            except (ValueError, TypeError):
+                continue
+
+    gmap = gmplot.GoogleMapPlotter(estacoes_filtradas[0][0], estacoes_filtradas[0][1], 10, apikey=GOOGLE_API_KEY)
+
+    for lat, lng, title, tipo in estacoes_filtradas:
+        if tipo == "Fluviometrica":
+            gmap.marker(lat, lng, title=title, color='red')
+        elif tipo == "Pluviometrica":
+            gmap.marker(lat, lng, title=title, color='blue')
+        
+    mapa_path = "html/index.html"
+    gmap.draw(mapa_path)
+    script_cru = '''
+    setTimeout(function() {
+        console.log('Mapa carregado');
+        document.querySelectorAll('div[role="img"]').forEach(function(div) {
+            div.addEventListener('click', function() {
+                const title = div.getAttribute('title') || 'Sem título';
+                navigator.clipboard.writeText(title).then(function() {
+                    alert('Código da estação copiado: ' + title);
+                }, function(err) {
+                    console.error('Erro ao copiar: ', err);
+                });
+            });
+        });
+    }, 5000);
+    '''
+    with open(mapa_path, 'r', encoding='utf-8') as file:
+        soup = BeautifulSoup(file, 'html.parser')
+
+    script_onclick = soup.new_tag('script', type='text/javascript')
+    script_onclick.string = script_cru
+
+    if soup.body:
+        soup.body.append(script_onclick)
+    else:
+        print("A tag <body> não foi encontrada no arquivo HTML.")
+
+    with open(mapa_path, 'w', encoding='utf-8') as file:
+        file.write(str(soup))
+
+    print(f"Mapa gerado com sucesso: {mapa_path}")
